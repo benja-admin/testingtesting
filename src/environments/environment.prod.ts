95399c20b96eb639a0f2efcedfebb63f3f398e97
= <=>
2020-05-13 21:04:08 -0400

    2nd commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBVrES06zvYgs38r0GIRYNo6mlEhQRa6M8',
+    authDomain: 'weds-619a7.firebaseapp.com',
+    databaseURL: 'https://weds-619a7.firebaseio.com',
+    projectId: 'weds-619a7',
+    storageBucket: 'weds-619a7.appspot.com',
+    messagingSenderId: '639873385047',
+    appId: '1:639873385047:web:c5b4f072f39d5642ed5912',
+    measurementId: 'G-2F5EBWYK61'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBVrES06zvYgs38r0GIRYNo6mlEhQRa6M8',
+    authDomain: 'weds-619a7.firebaseapp.com',
+    databaseURL: 'https://weds-619a7.firebaseio.com',
+    projectId: 'weds-619a7',
+    storageBucket: 'weds-619a7.appspot.com',
+    messagingSenderId: '639873385047',
+    appId: '1:639873385047:web:c5b4f072f39d5642ed5912',
+    measurementId: 'G-2F5EBWYK61'
+  }
 };
 
 /*
